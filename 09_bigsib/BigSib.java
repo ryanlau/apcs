651607ae31a2a97c961eb83282139b5e01b7781e
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2021-10-01 12:31:46 -0400

    add 09

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,10 @@
+// Ryan Lau and Jon
+// APCS
+// HW09 -- What Are BigSibs Good For?
+// 2021-10-01
+
+public class BigSib {
+	public static void greet(String name) {
+		System.out.println("Why, hello there, " + name + ". How do you do?");
+	}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,19 @@
+// Ryan Lau and Jon
+// APCS
+// HW09 -- What Are BigSibs Good For?
+// 2021-10-01
+
+/*
+DISCOVERIES
+
+UNRESOLVED QUESTIONS
+
+*/
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+}
