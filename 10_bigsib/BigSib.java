d2fef19af1195c3bf1dd5a1fbe4a57736ecfb120
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2021-10-04 22:37:47 -0400

    add 10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,10 @@
+// TNPG: JR (Ryan Lau, Jun Hong Wang, Jon, and Bob)
+// APCS
+// HW10 -- Refactor Big Sib One
+// 2021-10-04
+
+public class BigSib {
+	public static String greet(String name) {
+		return "Why, hello there, " + name + ". How do you do?";
+	}
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,19 @@
+// TNPG: JR (Ryan Lau, Jun Hong Wang, Jon, and Bob)
+// APCS
+// HW10 -- Refactor Big Sib One
+// 2021-10-04
+
+/*
+DISCOVERIES
+
+UNRESOLVED QUESTIONS
+
+*/
+
+public class Greet {
+    public static void main( String[] args ) {
+        System.out.println(BigSib.greet("Flim"));
+        System.out.println(BigSib.greet("Flam"));
+        System.out.println(BigSib.greet("Kazaam"));
+    }
+}
