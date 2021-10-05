c5e4ba6b92717168d9b51d12030884c1254942b4
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2021-10-05 16:07:59 -0400

    add 12

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,16 @@
+// Ryan Lau and Brian Kang
+// APCS
+// HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+// 2021-10-05
+
+public class BigSib {
+    static String helloMsg;
+
+	public static String greet(String name) {
+		return helloMsg + " " + name;
+	}
+
+    public static void setHelloMsg(String newHelloMsg) {
+        helloMsg = newHelloMsg;
+    }
+}
