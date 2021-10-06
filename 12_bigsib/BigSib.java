3630306aa2aa78e0cba9d34c8850f6887505799e
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2021-10-05 22:05:56 -0400

    add qcc and team name

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,4 +1,4 @@
-// Ryan Lau and Brian Kang
+// TNPG: Orion (Ryan Lau and Brian Kang)
 // APCS
 // HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
 // 2021-10-05
@@ -6,9 +6,9 @@
 public class BigSib {
     static String helloMsg;
 
-	public static String greet(String name) {
-		return helloMsg + " " + name;
-	}
+    public static String greet(String name) {
+        return helloMsg + " " + name;
+    }
 
     public static void setHelloMsg(String newHelloMsg) {
         helloMsg = newHelloMsg;
--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,4 +1,4 @@
-// Ryan Lau and Brian Kang
+// TNPG: Orion (Ryan Lau and Brian Kang)
 // APCS
 // HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
 // 2021-10-05
@@ -7,29 +7,30 @@
 DISCOVERIES
 
 UNRESOLVED QUESTIONS
-
+    0. Why does the property helloMsg need to be static in BigSib.java?
+    1. If a property doesn’t have a public or private signature, then what signature does it have?
 */
 
 public class Greet {
-  public static void main( String[] args ) {
-    String greeting;
+    public static void main(String[] args) {
+        String greeting;
 
-    BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
+        BigSib richard = new BigSib();
 
-    greeting = richard.greet("freshman");
-    System.out.println(greeting);
+        richard.setHelloMsg("Word up");
+        greeting = richard.greet("freshman");
+        System.out.println(greeting);
 
-    richard.setHelloMsg("Salutations");
-    greeting = richard.greet("Dr. Spaceman");
-    System.out.println(greeting);
+        richard.setHelloMsg("Salutations");
+        greeting = richard.greet("Dr. Spaceman");
+        System.out.println(greeting);
 
-    richard.setHelloMsg("Hey ya");
-    greeting = richard.greet("Kong Fooey");
-    System.out.println(greeting);
+        richard.setHelloMsg("Hey ya");
+        greeting = richard.greet("Kong Fooey");
+        System.out.println(greeting);
 
-    richard.setHelloMsg("Sup");
-    greeting = richard.greet("mom");
-    System.out.println(greeting);
-  }
+        richard.setHelloMsg("Sup");
+        greeting = richard.greet("mom");
+        System.out.println(greeting);
+    }
 }
