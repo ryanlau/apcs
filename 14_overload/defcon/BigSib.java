e5ddf37c1ddd48d8b0339273e05768d5941053c9
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2021-10-07 20:55:01 -0400

    add disco/qcc and team name

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -1,7 +1,17 @@
-// TNPG: ??? (Ryan Lau, Brian Kang, Kaitlin Ho, Jon, Vivian, and Apple)
+// TNPG: BARK (Ryan Lau, Brian Kang, Kaitlin Ho, Jon, Vivian, and Apple)
 // APCS
 // HW14 -- Customize Your Creation
 // 2021-10-07
+
+/*
+DISCOVERIES
+    0. Things in the default constructor can be overwritten 
+    1. static before a variable means that it is shared across all instances of the class
+        e.g. static String helloMsg would mean that the last new BigSib instance created (in this case it was "Salutations") would be used as the helloMsg for all 4 BigSibs 
+
+UNRESOLVED QUESTIONS
+    0. What is the purpose of making a method static versus making it nonstatic?
+*/
 
 public class BigSib {
     private String helloMsg;
