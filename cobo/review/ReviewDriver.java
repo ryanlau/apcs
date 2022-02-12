bb30615789792d3fef55b5554a5a465caea055c8
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2022-02-12 18:46:19 -0500

    complete activities 2 and 3 of lab03

--- cobo/review/ReviewDriver.java
+++ cobo/review/ReviewDriver.java
@@ -1,9 +1,22 @@
+// Team Gnocchi -- Hugo Jenkins (Boary), Ryan Lau (Jon), and Brian Kang (Vivian)
+// APCS pd6
+// L06 -- Read/Review/Expand
+// 2022-02-11f
+// time spent: .5hrs
+
 public class ReviewDriver {
     public static void main(String[] args) {
+        // activity uno
         System.out.println(Review.sentimentVal("hello"));
         System.out.println(Review.sentimentVal("bob"));
         System.out.println(Review.sentimentVal("ryan"));
 
+        // activity dos
         System.out.println(Review.totalSentiment("SimpleReview.txt"));
+        System.out.println(Review.starRating("SimpleReview.txt"));
+
+        // activity tres
+        System.out.println("team gnocchi !".indexOf(" "));
+        System.out.println(Review.fakeReview("SimpleReview.txt"));
     }
 }
